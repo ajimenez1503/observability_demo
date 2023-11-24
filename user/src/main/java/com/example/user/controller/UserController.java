fe4829d0373090ae34d86320751797e735251b2a
Antonio Jimenez <softwarejimenezz@gmail.com>
2023-11-24 18:12:07 +0100

    Add logs to user

--- user/src/main/java/com/example/user/controller/UserController.java
+++ user/src/main/java/com/example/user/controller/UserController.java
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.user.domain.User;
 
+import lombok.extern.slf4j.Slf4j;
 
 
+@Slf4j
 @RestController
 public class UserController {
 
     @GetMapping("users/{userId}")
     public User getUser(@PathVariable String userId) {
+        log.info("get User {} ", userId);
         return new User(userId, "name");
     }
 }
